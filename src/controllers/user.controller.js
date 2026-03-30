6186b6b662b0c16463d13b2ca913b723fc10009b
FonsahPageo <ashprincepageo@gmail.com>
2026-03-30 15:55:45 +0100

    logout user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -73,4 +73,28 @@ const loginUser = async (req, res) => {
   }
 };
 
-export { registerUser, loginUser };
+const logoutUser = async (req, res) => {
+  try {
+    const {email} = req.body;
+
+    const user = await User.findOne({
+      email: email.toLowerCase()
+    });
+
+    if(!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    return res.status(200).json({
+      message: "Logout successful"
+    })
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error"
+    })
+  }
+}
+
+export { registerUser, loginUser, logoutUser };
