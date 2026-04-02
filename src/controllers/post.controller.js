901ea935ae25960848b2783edbf4103f6da19a05
FonsahPageo <ashprincepageo@gmail.com>
2026-04-02 12:14:44 +0100

    update a post

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -34,16 +34,43 @@ const getPosts = async (req, res) => {
     const posts = await Post.find();
     return res.status(200).json({
       message: "Posts retrieved successfully",
-      posts
+      posts,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+const updatePosts = async (req, res) => {
+  try {
+    // basic validation to check if body is empty
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        message: "No data provided to update",
+      });
+    }
+
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Post updated successfully",
+      post
     })
   } catch (error) {
     return res.status(500).json({
       message: "Internal server error"
     })
   }
-}
-
-export {
-    createPost,
-    getPosts
 };
+
+export { createPost, getPosts, updatePosts };
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,9 +1,10 @@
 import { Router} from "express";
-import {createPost, getPosts} from "../controllers/post.controller.js";
+import {createPost, getPosts, updatePosts} from "../controllers/post.controller.js";
 
 const router = Router();
 
-router.route("/create").post(createPost);
+router.route("/create-post").post(createPost);
 router.route("/get-posts").get(getPosts);
+router.route("/update-posts/:id").patch(updatePosts);
 
 export default router;
